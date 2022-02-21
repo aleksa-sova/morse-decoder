5ce9169e77546f13cb103e1d490b4ee6033a0fae
aleksa-sova <aleksandra13sova@gmail.com>
2022-02-21 14:35:18 +0200

    fourth commit

--- src/index.js
+++ src/index.js
@@ -37,30 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-  let  MORSE_TABLE2 = Object.keys(MORSE_TABLE);
+// function decode(expr) {
+//   let  MORSE_TABLE2 = Object.keys(MORSE_TABLE);
   
-  let result = [];
+//   let result = [];
   
-//   let refinedData = MORSE_TABLE2.split('   ');
-  for (let i = 0; i < MORSE_TABLE2.length; i++) {
-      let temp = MORSE_TABLE2[i].split(' ');
-    //   temp = temp.trim();
-      for (let j = 0; j < temp.length; j++) {
-      if (MORSE_TABLE2[temp[j]]) {
-        result.push(MORSE_TABLE2[temp[j]]);
-      }
-    }
+// //   let refinedData = MORSE_TABLE2.split('   ');
+//   for (let i = 0; i < MORSE_TABLE2.length; i++) {
+//       let temp = MORSE_TABLE2[i].split(' ');
+//     //   temp = temp.trim();
+//       for (let j = 0; j < temp.length; j++) {
+//       if (MORSE_TABLE2[temp[j]]) {
+//         result.push(MORSE_TABLE2[temp[j]]);
+//       }
+//     }
     
-    if (i !== MORSE_TABLE2.length - 1) {
-    result.push(' ');
-    }
-  }
-  console.log(result)
-  return result.join('');
-}
+//     if (i !== MORSE_TABLE2.length - 1) {
+//     result.push(' ');
+//     }
+//   }
+//   console.log(result)
+//   return result.join('');
+// }
 
-decode("00000000100000111010101010111100111011100000001011111110101011111010101010101010")
-module.exports = {
-    decode
-}
+// decode("00000000100000111010101010111100111011100000001011111110101011111010101010101010")
+// module.exports = {
+//     decode
+// }
+
+console.log(MORSE_TABLE['.-']);
+console.log(MORSE_TABLE['-...']);
+console.log(MORSE_TABLE['-.-.']);
+console.log(MORSE_TABLE['-..']);
+console.log(MORSE_TABLE['.']);
+console.log(MORSE_TABLE['..-.']);
+console.log(MORSE_TABLE['--.']);
+console.log(MORSE_TABLE['....']);
+console.log(MORSE_TABLE['..']);
+console.log(MORSE_TABLE['.---']);
